cb7cf36fc76365117fcf818d91571bdde2eb308d
Austin T. Conn <atc3030dev@gmail.com>
2015-10-23 21:56:48 -0400

    Settings: Forward Port Volume Long Press Skip Tracks (1/2) Bring Forward Volume Rocker Track Change
    adapted to nougat without ignoring the mUseTvRouting that everyone else is skipping by @beanstown106
    
    Change-Id: I5147f39ec386c99690ab1aa17c728106fc982459

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3966,6 +3966,12 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
 
         /**
+         * Whether or not volume button music controls should be enabled to seek media tracks
+         * @hide
+         */
+        public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
+
+        /**
          * Whether to wake the screen with the volume keys, the value is boolean.
          * @hide
          */
