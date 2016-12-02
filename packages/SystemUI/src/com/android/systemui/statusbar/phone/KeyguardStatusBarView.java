470aa92c1f4dfcac007893f762c02a42c8454186
tsubus <tsubus@ilwt.org>
2016-12-02 00:04:19 -0500

    KeyguardStatusBarView: Fix battery text visibility issues
    Change-Id: I8f517ad38f9c225b69eeadffa71efd811a523f78

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -33,6 +33,7 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.android.systemui.BatteryMeterDrawable;
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
@@ -53,6 +54,8 @@ public class KeyguardStatusBarView extends RelativeLayout
 
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT =
             Settings.Secure.STATUS_BAR_SHOW_BATTERY_PERCENT;
+    private static final String STATUS_BAR_BATTERY_STYLE =
+            Settings.Secure.STATUS_BAR_BATTERY_STYLE;
 
     private boolean mBatteryCharging;
     private boolean mKeyguardUserSwitcherShowing;
@@ -74,6 +77,7 @@ public class KeyguardStatusBarView extends RelativeLayout
     private View mSystemIconsContainer;
 
     private boolean mShowBatteryText;
+    private Boolean mForceBatteryText;
 
     private ContentObserver mObserver = new ContentObserver(new Handler()) {
         public void onChange(boolean selfChange, Uri uri) {
@@ -191,8 +195,12 @@ public class KeyguardStatusBarView extends RelativeLayout
                 mMultiUserSwitch.setVisibility(View.GONE);
             }
         }
-        mBatteryLevel.setVisibility(
-                mBatteryCharging || mShowBatteryText ? View.VISIBLE : View.GONE);
+        if (mForceBatteryText != null) {
+            mBatteryLevel.setVisibility(mForceBatteryText ? View.VISIBLE : View.GONE);
+        } else {
+            mBatteryLevel.setVisibility(
+                    mBatteryCharging || mShowBatteryText ? View.VISIBLE : View.GONE);
+        }
 
         if (mCarrierLabel != null) {
             if (mShowCarrierLabel == 1) {
@@ -227,7 +235,8 @@ public class KeyguardStatusBarView extends RelativeLayout
         }
         mBatteryListening = listening;
         if (mBatteryListening) {
-            TunerService.get(getContext()).addTunable(this, STATUS_BAR_SHOW_BATTERY_PERCENT);
+            TunerService.get(getContext()).addTunable(this,
+                    STATUS_BAR_SHOW_BATTERY_PERCENT, STATUS_BAR_BATTERY_STYLE);
             mBatteryController.addStateChangedCallback(this);
         } else {
             mBatteryController.removeStateChangedCallback(this);
@@ -375,9 +384,23 @@ public class KeyguardStatusBarView extends RelativeLayout
 
     @Override
     public void onTuningChanged(String key, String newValue) {
-        if (key.equals(STATUS_BAR_SHOW_BATTERY_PERCENT)) {
-            mShowBatteryText = newValue == null ? false : Integer.parseInt(newValue) == 2;
-            updateVisibilities();
+        switch (key) {
+            case STATUS_BAR_SHOW_BATTERY_PERCENT:
+                mShowBatteryText = newValue != null && Integer.parseInt(newValue) == 2;
+                break;
+            case STATUS_BAR_BATTERY_STYLE:
+                if (newValue != null) {
+                    final int value = Integer.parseInt(newValue);
+                    if (value == BatteryMeterDrawable.BATTERY_STYLE_TEXT) {
+                        mForceBatteryText = true;
+                    } else if (value == BatteryMeterDrawable.BATTERY_STYLE_HIDDEN) {
+                        mForceBatteryText = false;
+                    } else {
+                        mForceBatteryText = null;
+                    }
+                }
+                break;
         }
+        updateVisibilities();
     }
 }
